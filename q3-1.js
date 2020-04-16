14eeb46ebb469c9cecde85f6b1784fcafc68f0c8
kimgoing <61349991+kimgoing@users.noreply.github.com>
2020-04-16 20:52:23 +0900

    Add files via upload

---
+++ q3-1.js
@@ -0,0 +1,58 @@
+/* --- second try
+function changeImage() {
+  var element = document.getElementsByClassName("clap");
+  var style = window.getComputedStyle(element);
+  var index = style.getPropertyValue("z-index");
+
+  if(index > 0)
+  {
+    document.getElementsByClassName("clap").style.zIndex = "-1";
+  }
+  else {
+    document.getElementsByClassName("clap").style.zIndex = "1";
+  }
+}
+*/
+
+/* --- first try
+document.getElementsByClassName('clapping-container').ontouchstart = changeImage;
+
+function changeImage() {
+  document.getElementsByClassName('clap_2').style.z-index = 1;
+}
+*/
+
+/*
+function changeImage() {
+  var element = document.getElementsByClassName('clap');
+  var index = object.getPropertyValue('z-index');
+
+  if(index > 0)
+  {
+    document.getElementsByClassName('clap').style.zIndex = "-1";
+  }
+  else
+  {
+    document.getElementsByClassName('clap').style.zindex = '1';
+  }
+}
+*/
+
+/* --- third try
+document.getElementsByClassName("clap").addEventListener("touchstart", myFunction);
+
+function myFunction {
+  document.getElementsByClassName("clap").style
+}
+*/
+
+/* --- forth try
+document.getElementsByClassName("clapping-container").ontouchstart = myFunction;
+
+function myFunction() {
+  style.getPropertyValue("z-index") = -1;
+}
+*/
+
+document.getElementsByClassName("clapping-container").addEventListener("click", function(){
+document.getElementsByClassName("clap").style.opacity  =   document.getElementById("clap").style.opacity*0; });
